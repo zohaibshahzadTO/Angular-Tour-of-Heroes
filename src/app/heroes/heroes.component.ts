edf1d45db84fbb04f8c660ffc9c8f9c5d84e32a4
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-01-24 15:26:00 -0500

    Hero Component Work
    Added css private styles within the Hero Component, Exported more heroes to increase the roster, and implemented the *ngFor directive in the html.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,9 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
