4a2b0b23553270ac63bd9e1190a5b85e3e11478e
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-01-26 16:57:31 -0500

    Added the @Input() hero property
    The HeroDetailComponent template binds to the component's hero property which is of type Hero.
    
    We opened the HeroDetailComponent class file and import the Hero symbol.
    
    The hero property must be an Input property, annotated with the @Input() decorator, because the external HeroesComponent will bind to it like this.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,6 +7,8 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
 
   constructor() { }
 
