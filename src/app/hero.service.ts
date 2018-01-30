024ee1053c3b53577dd913e9ebf76e2761d09c7b
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-01-30 13:58:36 -0500

    @Injectable Services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
 
 @Injectable()
 export class HeroService {
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 
   constructor() { }
 
